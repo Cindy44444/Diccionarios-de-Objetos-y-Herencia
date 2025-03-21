629f8f263c737eb42f77b24c1249ba2054815330
Lab2-13 <Lab2-13@LAB2-13>
2025-03-20 18:44:04 -0600

    cambio

---
+++ ControllerVehículo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionarios_de_Objetos_y_Herencia
+{
+    internal class ControllerVehículo
+    {
+        Dictionary<int, VehículoAuto> diccionarioCoche = new Dictionary<int, VehículoAuto>();
+        Dictionary<int, VehículoMotos> diccionarioMotos = new Dictionary<int, VehículoMotos>();
+        VehículoAuto vau = new VehículoAuto();
+        VehículoMotos vmo = new VehículoMotos();
+
+        public void AddAuto()
+        {
+            Console.WriteLine("Dame iD");
+            int iD = Convert.ToInt32(Console.ReadLine());
+            vau.ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame Marca");
+            vau.Marca = Console.ReadLine();
+            Console.WriteLine("Dame Modelo");
+            vau.Modelo = Console.ReadLine();
+            Console.WriteLine("Dame Año");
+            vau.Año = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame Número de puertas");
+            vau.Numpuertas= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame color");
+            vau.Color = Console.ReadLine();
+           
+
+            diccionarioCoche.Add(iD, new VehículoAuto(vau.ID,vau.Marca,vau.Modelo,vau.Año,vau.Numpuertas,vau.Color));
+        }
+        public void AddMoto()
+        {
+            Console.WriteLine("Dame iD");
+            int iD = Convert.ToInt32(Console.ReadLine());
+            vmo.ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame Marca");
+            vmo.Marca = Console.ReadLine();
+            Console.WriteLine("Dame Modelo");
+            vmo.Modelo = Console.ReadLine();
+            Console.WriteLine("Dame Año");
+            vmo.Año = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame Escape");
+            vmo.Escape = Console.ReadLine();
+            Console.WriteLine("Dame Motor");
+            vmo.Motor = Console.ReadLine();
+
+            diccionarioMotos.Add(iD, new VehículoMotos(vmo.ID, vmo.Marca, vmo.Modelo, vmo.Año, vmo.Escape, vmo.Motor));
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace Diccionarios_de_Objetos_y_Herencia
 {
+    public enum Menu
+    {
+        Agregar=1, Mostrar, Actualizar, Eliminar
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
 
         }
+    }
+    static Menu menu()
+    {
+        Console.WriteLine("Agregar");
+        Console.WriteLine("Mostrar vehículos");
+        Console.WriteLine("Modificar Vehículos");
+        Console.WriteLine("Eliminar Vehículos");
+        Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+        return opc;
     }
 }
