cb5e5d22e8ba8ca81951950d5394b4ca8f5dd3ec
Lab2-13 <Lab2-13@LAB2-13>
2025-03-25 11:05:21 -0600

    menu ahhhh

--- Program.cs
+++ Program.cs
@@ -8,23 +8,33 @@ namespace Diccionarios_de_Objetos_y_Herencia
 {
     public enum Menu
     {
-        Agregar=1, Mostrar, Actualizar, Eliminar
+        AgregarCoche=1,AgregarMoto, MostrarCoche,MostrarMoto, ActualizarCoche,ActualizarMoto, EliminarCoche,EliminarMoto
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                switch(Menu())
+                {
 
+                }
+            }
+        }
+        static Menu menu()
+        {
+            Console.WriteLine("Agregar Coche:");
+            Console.WriteLine("Agregar Moto:");
+            Console.WriteLine("Mostrar vehículos");
+            Console.WriteLine("Modificar Coche:");
+            Console.WriteLine("Modificar Moto:");
+            Console.WriteLine("Eliminar Coche:");
+            Console.WriteLine("Eliminar Moto:");
+            Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
+            return opc;
         }
     }
-    static Menu menu()
-    {
-        Console.WriteLine("Agregar");
-        Console.WriteLine("Mostrar vehículos");
-        Console.WriteLine("Modificar Vehículos");
-        Console.WriteLine("Eliminar Vehículos");
-        Menu opc = (Menu)Convert.ToInt32(Console.ReadLine());
-        return opc;
-    }
+    
 }
